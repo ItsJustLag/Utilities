455e1f7caf3c42c9d13369e5fa13384436529655
stalker0000 <stalker0000@users.noreply.github.com>
2021-02-15 14:17:33 -0500

    Update Downloads_cleaner.py

--- Utilities/Downloads_cleaner.py
+++ Utilities/Downloads_cleaner.py
@@ -15,28 +15,44 @@ def choice():
 def main():
     removed = int(0)
     failed = int(0)
-    for file in os.listdir("C:/users/" + os.getlogin() + "/Downloads"):
-        if file.lower().endswith(".exe"):
-            try:
-                os.remove("C:/users/" + os.getlogin() + "/Downloads/" + file)
-                print(file + " removed")
-                removed += 1
-                time.sleep(0.2)
-            except:
-                print("failed to remove " + file)
-                failed += 1
-                time.sleep(0.2)
+
+    if os.path.exists("C:/users/" + os.getlogin()):
+        for file in os.listdir("C:/users/" + os.getlogin() + "/Downloads/"):
+            if file.lower().endswith(".exe") or file.lower().endswith(".msi"):
+                try:
+                    os.remove("C:/users/" + os.getlogin() + "/Downloads/" + file)
+                    print(file + " removed")
+                    removed += 1
+                    time.sleep(0.2)
+                except:
+                    print("failed to remove " + file)
+                    failed += 1
+                    time.sleep(0.2)
+    else:
+        users = os.listdir("C:/users/")
+        print("Which is your user folder?")
+
+        for user in users:
+            print(user)
         
-        elif file.lower().endswith(".msi"):
-            try:
-                os.remove("C:/users/" + os.getlogin() + "/Downloads/" + file)
-                print(file + " removed")
-                removed += 1
-                time.sleep(0.2)
-            except:
-                print("failed to remove " + file)
-                failed += 1
-                time.sleep(0.2)
+        user = input("Which is your user folder? ")
+
+        while not user in users:
+            user = input("Which is your user folder? ")
+
+        for file in os.listdir(f"C:/users/{user}/Downloads/"):
+            if file.lower().endswith(".exe") or file.lower().endswith(".msi"):
+                try:
+                    os.remove("C:/users/" + user + "/Downloads/" + file)
+                    print(file + " removed")
+                    removed += 1
+                    time.sleep(0.2)
+                except:
+                    print("failed to remove " + file)
+                    failed += 1
+                    time.sleep(0.2)
+        
+
     removed = str(removed)
     failed = str(failed)
     time.sleep(1)
